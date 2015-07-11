44f883dca42f062859cecad6c0e047fa6000efd8
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    msm: mdss: LiveDisplay driver
    * Bring in a lightweight version of the LiveDisplay driver which
      works for this chipset, and remove all the OEM spaghetti code.
    * This version of the driver does not include the generic
      RGB offset postprocessing, since we're going to do this using
      the official QDCM APIs.
    
    Change-Id: Ifcaaf93d56933d019d1f469fd70b8466cd4ad3e7
    Signed-off-by: Steve Kondik <steve@cyngn.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -34,6 +34,7 @@
 #include "mdss_debug.h"
 #include "mdss_dsi_phy.h"
 #include "mdss_dba_utils.h"
+#include "mdss_livedisplay.h"
 #ifdef CONFIG_MACH_ASUS_SDM660
 #include "mdss_panel.h"
 #endif
@@ -3154,6 +3155,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		}
 		break;
 #endif
+	case MDSS_EVENT_UPDATE_LIVEDISPLAY:
+		rc = mdss_livedisplay_update(ctrl_pdata, (int)(unsigned long) arg);
+		break;
 	default:
 		pr_debug("%s: unhandled event=%d\n", __func__, event);
 		break;

316f5f785b812ddabab91e14e08c2b3d203cda24
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 01:15:15 +0900

    drivers: dma-buf: Use kmem_cache pool for struct dma_buf_attachment
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- init/main.c
+++ init/main.c
@@ -528,6 +528,8 @@ static void __init mm_init(void)
 	kaiser_init();
 }
 
+void __init init_dma_buf_kmem_pool(void);
+
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -719,6 +721,7 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
+	init_dma_buf_kmem_pool();
 
 	check_bugs();
 

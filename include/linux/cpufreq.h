2b511523697bd70c00aee77dcbb833f17006e6f5
Oktapra Amtono <oktapra.amtono@gmail.com>
2019-12-09 12:04:07 +0700

    sched/cpufreq: Add energy_dcfc governor
    diff from 'https://github.com/kdrag0n/velocity_tissot/commit/1b8e8b5e8f3ad15307903080ff202105d2428bb4'
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.co

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -547,6 +547,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ENERGY)
+extern struct cpufreq_governor cpufreq_gov_energy_dcfc;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_energy_dcfc)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)

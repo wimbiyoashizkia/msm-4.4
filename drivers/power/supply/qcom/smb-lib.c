7670e2b223fa8c231a23461257c5a49b0cabc68b
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-06-10 01:11:04 +0700

    drivers/power: supply: Remove wakelock
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -31,7 +31,6 @@
 #include "fg-core.h"
 #include <linux/gpio.h>
 #include <linux/alarmtimer.h>
-#include <linux/wakelock.h>
 #include <linux/unistd.h>
 #include <linux/fcntl.h>
 #include <linux/slab.h>
@@ -109,17 +108,6 @@ static void asus_smblib_rerun_aicl(struct smb_charger *chg)
 	/* reg=1380, bit2=0, USBIN_AICL_EN=enable */
 	smblib_masked_write(chg, USBIN_AICL_OPTIONS_CFG_REG,
 				USBIN_AICL_EN_BIT, USBIN_AICL_EN_BIT);
-}
-
-extern struct wake_lock asus_chg_lock;
-void asus_smblib_stay_awake(struct smb_charger *chg)
-{
-	wake_lock(&asus_chg_lock);
-}
-
-void asus_smblib_relax(struct smb_charger *chg)
-{
-	wake_unlock(&asus_chg_lock);
 }
 #endif
 
@@ -831,8 +819,6 @@ static void smblib_uusb_removal(struct smb_charger *chg)
 
 	alarm_cancel(&bat_alarm);
 	asus_flow_processing = 0;
-
-	asus_smblib_relax(smbchg_dev);
 #endif
 }
 
@@ -1560,7 +1546,6 @@ static int _smblib_vbus_regulator_enable(struct regulator_dev *rdev)
 	}
 #ifdef CONFIG_MACH_ASUS_X00T
 	smblib_asus_monitor_start(smbchg_dev, 10000);
-	asus_smblib_stay_awake(chg);
 #endif
 	return rc;
 }
@@ -1623,8 +1608,8 @@ static int _smblib_vbus_regulator_disable(struct regulator_dev *rdev)
 	cancel_delayed_work(&chg->asus_min_monitor_work);
 	cancel_delayed_work(&chg->asus_batt_RTC_work);
 	alarm_cancel(&bat_alarm);
-	asus_smblib_relax(smbchg_dev);
 #endif
+
 	rc = smblib_masked_write(chg, OTG_ENG_OTG_CFG_REG,
 				 ENG_BUCKBOOST_HALT1_8_MODE_BIT, 0);
 	if (rc < 0) {
@@ -3799,8 +3784,6 @@ void asus_min_monitor_work(struct work_struct *work)
 					msecs_to_jiffies(ASUS_MONITOR_CYCLE));
 		schedule_delayed_work(&smbchg_dev->asus_batt_RTC_work, 0);
 	}
-
-	asus_smblib_relax(smbchg_dev);
 }
 
 /* ASUS BSP: Add per min monitor jeita & thermal & typeC_DFP */
@@ -3941,7 +3924,6 @@ void asus_chg_flow_work(struct work_struct *work)
 		break;
 
 	default:
-		asus_smblib_relax(smbchg_dev);
 		break;
 	}
 }
@@ -4333,7 +4315,6 @@ static void smblib_micro_usb_plugin(struct smb_charger *chg, bool vbus_rising)
 		if (!asus_flow_processing) {
 			asus_flow_processing = 1;
 			asus_insertion_initial_settings(smbchg_dev);
-			asus_smblib_stay_awake(smbchg_dev);
 			schedule_delayed_work(&smbchg_dev->asus_chg_flow_work,
 						msecs_to_jiffies(12000));
 			asus_update_usb_connector_state(smbchg_dev);

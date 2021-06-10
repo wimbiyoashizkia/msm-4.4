2aa60744059d651d96dbbbadad4dbdecef7512ea
yarost12 <yaro330@gmail.com>
2021-06-10 17:05:29 +0700

    adrenoboost: we don't actually need to pass flags to adrenoboost_debug
    Forgot to remove it but it's fine.
    Signed-off-by: YaroST12 <yaro330@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -385,7 +385,7 @@ extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq
 
 #if 1
 static int adrenoboost_debug(struct devfreq *devfreq, unsigned long *freq,
-				u32 *flag, int jump_dir);
+				int jump_dir);
 
 /*
 * mapping gpu level calculated linear conservation half curve
@@ -543,7 +543,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		level += val;
 		level = max(level, 0);
 		level = min_t(int, level, devfreq->profile->max_state - 1);
-		adrenoboost_debug(devfreq, freq, flag, 0);
+		adrenoboost_debug(devfreq, freq, 0);
 		priv->bin.last_level = level;
 	} else {
 		if (val) {
@@ -557,7 +557,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 			 */
 			if (val<0 && priv->bin.cycles_keeping_level <
 				conservation_map_up[ last_level ]) {
-				adrenoboost_debug(devfreq, freq, flag, 1);
+				adrenoboost_debug(devfreq, freq, 1);
 			} else
 
 			/*
@@ -566,7 +566,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 			 */
 			if (val>0 && priv->bin.cycles_keeping_level <
 				conservation_map_down[ last_level ])  {
-				adrenoboost_debug(devfreq, freq, flag, 2);
+				adrenoboost_debug(devfreq, freq, 2);
 			} else {
 				level += val;
 				level = max(level, 0);
@@ -575,7 +575,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				priv->bin.cycles_keeping_level = 0;
 				// set new last level
 				priv->bin.last_level = level;
-				adrenoboost_debug(devfreq, freq, flag, 0);
+				adrenoboost_debug(devfreq, freq, 0);
 			}
 		}
 	}
@@ -596,7 +596,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 
 #if 1
 static int adrenoboost_debug(struct devfreq *devfreq, unsigned long *freq,
-				u32 *flag, int jump_dir)
+				int jump_dir)
 {
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
 	struct devfreq_dev_status stats;

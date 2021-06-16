080603007e0e93a8df0988acaa0d02b27b47c2f1
xNombre <kartapolska@gmail.com>
2021-06-16 12:08:12 +0700

    sound: Import Oneplus changes for 24bit audio
    This makes audio quality better.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- sound/soc/msm/qdsp6v2/msm-dai-stub-v2.c
+++ sound/soc/msm/qdsp6v2/msm-dai-stub-v2.c
@@ -92,13 +92,14 @@ static struct snd_soc_dai_driver msm_dai_stub_dai_rx = {
 	.playback = {
 		.stream_name = "Stub Playback",
 		.aif_name = "STUB_RX",
-		.rates = SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_8000 |
+		.rates = SNDRV_PCM_RATE_192000 | SNDRV_PCM_RATE_96000 | 
+			SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_8000 |
 			SNDRV_PCM_RATE_16000,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE,
 		.channels_min = 1,
 		.channels_max = 2,
 		.rate_min = 8000,
-		.rate_max = 48000,
+		.rate_max = 192000,
 	},
 	.ops = &msm_dai_stub_ops,
 	.probe = &msm_dai_stub_dai_probe,
@@ -112,7 +113,7 @@ static struct snd_soc_dai_driver msm_dai_stub_dai_tx[] = {
 			.aif_name = "STUB_TX",
 			.rates = SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_8000 |
 				SNDRV_PCM_RATE_16000,
-			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE,
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min = 8000,

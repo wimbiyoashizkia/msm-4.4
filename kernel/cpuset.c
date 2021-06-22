cdb192c62f7168479e75a08bb5dfb1a34d7f77a8
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-06-22 15:47:14 +0700

    uclamp_assist: Tune values
    These values were tested on an sdm660 device, results are as follows:
    
    Jitter:          0.168 ms
    CPU Duration:   15.669 ms
    UI Duration:     0.774 ms
    RT Duration:    14.896 ms
    Total duration: 18.760 ms
    
    And no major spikes were observed.
    These values were taken from the UIBench app benchmark.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2083,13 +2083,13 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",			"10", "100", 1, 1},
-		{"audio-app",			"10", "40", 0, 0},
-		{"foreground",			"0",  "50",  1, 0},
-		{"restricted",			"10", "40", 0, 0},
-		{"camera-daemon",	"10", "40", 0, 0},
-		{"background",		"20", "100", 0, 0},
-		{"system-background",	"0",  "40",  0, 0},
+		{"top-app",		"20", "max", 1, 0},
+		{"audio-app",		"0", "40", 0, 0},
+		{"foreground",		"10",  "100",  1, 0},
+		{"restricted",		"50", "100", 1, 0},
+		{"camera-daemon",	"10", "50", 0, 0},
+		{"background",		"20", "max", 1, 1},
+		{"system-background",	"20",  "100",  1, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {

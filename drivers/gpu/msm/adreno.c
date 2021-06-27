1d289ac204105acc91d4ef551b3fe950fc4b2dbc
Jordan Crouse <jcrouse@codeaurora.org>
2021-06-27 14:46:44 +0700

    msm: kgsl: Remove warm/cold start
    4xx was the only target that used "warm" start for bootstrap. Since that
    is now gone we can remove the rest of the infrastructure and clean things
    up a bit.
    
    Change-Id: Ic0dedbad701559ae9a448c1a674cd0c1aea0fbab
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1339,7 +1339,7 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	/* Clear FSR here in case it is set from a previous pagefault */
 	kgsl_mmu_clear_fsr(&device->mmu);
 
-	status = adreno_ringbuffer_start(adreno_dev, ADRENO_START_COLD);
+	status = adreno_ringbuffer_start(adreno_dev);
 	if (status)
 		goto error_mmu_off;
 
@@ -2112,10 +2112,7 @@ static int adreno_soft_reset(struct kgsl_device *device)
 	 * otherwise do a full ringbuffer restart
 	 */
 
-	if (ADRENO_FEATURE(adreno_dev, ADRENO_WARM_START))
-		ret = adreno_ringbuffer_start(adreno_dev, ADRENO_START_WARM);
-	else
-		ret = adreno_ringbuffer_start(adreno_dev, ADRENO_START_COLD);
+	ret = adreno_ringbuffer_start(adreno_dev);
 	if (ret == 0) {
 		device->reset_counter++;
 		set_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);

26bbcd74d871bda9dd8c25dd4c23b43801adfd5a
YaroST12 <yaro330@gmail.com>
2021-07-01 15:43:34 +0700

    synaptics_dsx_2.7: Add Synaptics DT2W support
    Signed-off-by: YaroST12 <yaro330@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_core.c
@@ -1345,8 +1345,10 @@ static int synaptics_rmi4_f11_abs_report(struct synaptics_rmi4_data *rmi4_data,
 
 		if (detected_gestures) {
 			input_report_key(rmi4_data->input_dev, KEY_WAKEUP, 1);
+			input_report_key(rmi4_data->input_dev, KEY_DOUBLE_TAP, 1);
 			input_sync(rmi4_data->input_dev);
 			input_report_key(rmi4_data->input_dev, KEY_WAKEUP, 0);
+			input_report_key(rmi4_data->input_dev, KEY_DOUBLE_TAP, 0);
 			input_sync(rmi4_data->input_dev);
 			rmi4_data->suspend = false;
 		}
@@ -1612,12 +1614,14 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 #else
 		if (gesture_type && gesture_type != F12_UDG_DETECT) {
 			input_report_key(rmi4_data->input_dev, KEY_WAKEUP, 1);
+			input_report_key(rmi4_data->input_dev, KEY_DOUBLE_TAP, 1);
 #endif /* CONFIG_MACH_ASUS_X00T */
 			input_sync(rmi4_data->input_dev);
 #ifdef CONFIG_MACH_ASUS_X00T
 			input_report_key(rmi4_data->input_dev, keycode, 0);
 #else
 			input_report_key(rmi4_data->input_dev, KEY_WAKEUP, 0);
+			input_report_key(rmi4_data->input_dev, KEY_DOUBLE_TAP, 0);
 #endif
 			input_sync(rmi4_data->input_dev);
 #ifdef CONFIG_MACH_ASUS_X00T
@@ -3797,6 +3801,7 @@ static void synaptics_rmi4_set_params(struct synaptics_rmi4_data *rmi4_data)
 
 	if (rmi4_data->f11_wakeup_gesture || rmi4_data->f12_wakeup_gesture) {
 		set_bit(KEY_WAKEUP, rmi4_data->input_dev->keybit);
+		set_bit(KEY_DOUBLE_TAP, rmi4_data->input_dev->keybit);
 #ifdef CONFIG_MACH_ASUS_X00T
 		input_set_capability(rmi4_data->input_dev, EV_KEY,
 					GESTURE_EVENT_DOUBLE_CLICK);
@@ -3832,6 +3837,7 @@ static void synaptics_rmi4_set_params(struct synaptics_rmi4_data *rmi4_data)
 					GESTURE_EVENT_SWIPE_RIGHT);
 #else
 		input_set_capability(rmi4_data->input_dev, EV_KEY, KEY_WAKEUP);
+		input_set_capability(rmi4_data->input_dev, EV_KEY, KEY_DOUBLE_TAP);
 #endif
 	}
 

78cef86878d857d5c2de70bd7dc50a5b76e1169f
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-07-04 08:54:45 +0700

    schedutil: Remove unused LATENCY_MULTIPLIER
    * Now LATENCY_MULTIPLIER has been replaced with b6db523469a297825a43ad8375dc02227f91f34e,
      So now it's not needed anymore
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -26,8 +26,6 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_driver_fast_switch(x, y) 0
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
-#define LATENCY_MULTIPLIER			(1000)
-
 #define DEFAULT_DOWN_TOLERANCE_LP (20 * NSEC_PER_MSEC)
 #define DEFAULT_DOWN_TOLERANCE_PERF (20 * NSEC_PER_MSEC)
 

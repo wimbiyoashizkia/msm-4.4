5a40edece5895138935dca81d553cd1b267018f1
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-07-04 23:33:26 +0700

    AnyKernel3: Add EAS Perf HAL
    * Now Patches EAS Perf HAL for module magisk
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -39,25 +39,17 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
+# begin EAS patch changes
+if [ ! -e "/vendor/etc/powerhint.json" ]; then
+    ui_print " " "HMP ROM Detected!"
+    ui_print "Installing Module EAS Perf HAL..."
+    rm -rf /data/adb/modules/neon;
+    cp -rf $home/patch/eas-perfhal /data/adb/modules/neon;
+else
+    ui_print " " "EAS ROM Detected!"
+    ui_print "No need Module EAS Perf HAL!!!"
+fi
+# end EAS patch changes
 
 write_boot;
 ## end boot install

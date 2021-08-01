393fc8b80509208bd10d2db0d9f53bcf88055878
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-08-01 21:16:09 +0700

    kernel/sched: tune: Add sched_boost tunable
    * Following da914bd41330e7c104a95772f1fd17d800066a31 after expose, need set on stune assist for value
      and  make appearing in dmesg log.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -866,16 +866,17 @@ struct st_data {
 	char *name;
 	int boost;
 	bool prefer_idle;
+	int sched_boost;
 };
 
 static void write_default_values(struct cgroup_subsys_state *css)
 {
 	static struct st_data st_targets[] = {
-		{ "audio-app",		0, 0 },
-		{ "background",		-25, 0 },
-		{ "foreground",		0, 1 },
-		{ "rt",			0, 0 },
-		{ "top-app",		1, 1 },
+		{ "audio-app",		0, 0, 0 },
+		{ "background",		-25, 0, 0 },
+		{ "foreground",		0, 1, 0 },
+		{ "rt",			0, 0, 0 },
+		{ "top-app",		1, 1, 0 },
 	};
 	int i;
 
@@ -883,11 +884,12 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		struct st_data tgt = st_targets[i];
 
 		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
-			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle);
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n sched_boost=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle, tgt.sched_boost);
 
 			boost_write(css, NULL, tgt.boost);
 			prefer_idle_write(css, NULL, tgt.prefer_idle);
+			sched_boost_write(css, NULL, tgt.sched_boost);
 		}
 	}
 }

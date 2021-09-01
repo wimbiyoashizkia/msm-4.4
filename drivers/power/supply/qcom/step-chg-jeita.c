eebaa40b97fda675ec34c070e7194638f7207e0f
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-09-01 22:41:38 +0700

    step-chg-jeita: Configure for FCC
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -88,9 +88,9 @@ static struct step_chg_cfg step_chg_config = {
 	.hysteresis	= 100000, /* 100mV */
 	.fcc_cfg	= {
 		/* VBAT_LOW	VBAT_HIGH	FCC */
-		{3600000,	4000000,	5000000},
+		{3600000,	4000000,	4500000},
 		{4001000,	4200000,	5000000},
-		{4201000,	4400000,	5000000},
+		{4201000,	4400000,	6000000},
 	},
 	/*
 	 *	SOC STEP-CHG configuration example.
@@ -121,10 +121,10 @@ static struct jeita_fcc_cfg jeita_fcc_config = {
 	.hysteresis	= 10, /* 1degC hysteresis */
 	.fcc_cfg	= {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
-		{0,		100,		5000000},
+		{0,		100,		4500000},
 		{101,		200,		5000000},
-		{201,		450,		5000000},
-		{451,		550,		5000000},
+		{201,		450,		5500000},
+		{451,		550,		6000000},
 	},
 };
 
@@ -134,9 +134,9 @@ static struct jeita_fv_cfg jeita_fv_config = {
 	.hysteresis	= 10, /* 1degC hysteresis */
 	.fv_cfg		= {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
-		{0,		100,		6200000},
-		{101,		450,		6200000},
-		{451,		550,		6200000},
+		{0,		100,		5000000},
+		{101,		450,		5500000},
+		{451,		550,		6000000},
 	},
 };
 

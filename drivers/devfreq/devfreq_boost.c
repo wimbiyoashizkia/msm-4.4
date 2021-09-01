18d2e6a18af3b8fc00b70ea393cc362c0ffb85c9
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-09-01 21:50:12 +0700

    Revert "devfreq_boost: Handle last_input_time if cpu input boost driver is disabled"
    This reverts commit f12d9b522da5683bec50764c6425e4b506159035.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -10,10 +10,6 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 
-#ifndef CONFIG_CPU_INPUT_BOOST
-	unsigned long last_input_time;
-#endif
-
 enum {
 	SCREEN_OFF,
 	INPUT_BOOST,
@@ -215,10 +211,6 @@ static void devfreq_boost_input_event(struct input_handle *handle,
 
 	for (i = 0; i < DEVFREQ_MAX; i++)
 		__devfreq_boost_kick(d->devices + i);
-
-#ifndef CONFIG_CPU_INPUT_BOOST
-	last_input_time = jiffies;
-#endif
 }
 
 static int devfreq_boost_input_connect(struct input_handler *handler,

0d49903c7e7f7a8796ad3b20d8c40126bb605aad
Antonio Quartulli <a@unstable.cc>
2021-09-09 11:31:40 +0700

    wireguard: global: avoid double unlikely() notation when using IS_ERR()
    The definition of IS_ERR() already applies the unlikely() notation
    when checking the error status of the passed pointer. For this
    reason there is no need to have the same notation outside of
    IS_ERR() itself.
    
    Clean up code by removing redundant notation.
    
    Signed-off-by: Antonio Quartulli <a@unstable.cc>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/net/wireguard/socket.c
+++ drivers/net/wireguard/socket.c
@@ -71,7 +71,7 @@ static int send4(struct wg_device *wg, struct sk_buff *skb,
 				ip_rt_put(rt);
 			rt = ip_route_output_flow(sock_net(sock), &fl, sock);
 		}
-		if (unlikely(IS_ERR(rt))) {
+		if (IS_ERR(rt)) {
 			ret = PTR_ERR(rt);
 			net_dbg_ratelimited("%s: No route to %pISpfsc, error %d\n",
 					    wg->dev->name, &endpoint->addr, ret);

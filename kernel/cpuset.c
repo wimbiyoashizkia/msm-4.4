ebcbfa7f073540040f4249e674c900410d56cdf9
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-11-03 18:58:25 +0700

    cpuset: Move tunables to defconfig
    Kang from Art-Chen/android_kernel_realme_sdm710@205255a
    
    * Change value default
    * Adapt for msm-4.4
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1789,13 +1789,13 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
 		/* Little-only cpusets go first */
-		{ "background",		"0-1" },
-		{ "audio-app",		"0-3"},
-		{ "camera-daemon",	"0-3" },
-		{ "system-background",	"0-3" },
-		{ "restricted",		"0-5" },
-		{ "top-app",		"0-7" },
-		{ "foreground",		"0-3,6-7" },
+		{ "background",	CONFIG_CPUSETS_BG },
+		{ "audio-app",		CONFIG_CPUSETS_AUDIO_APP },
+		{ "camera-daemon",	CONFIG_CPUSETS_CAMERA },
+		{ "system-background",	CONFIG_CPUSETS_SYSTEM_BG },
+		{ "restricted",	CONFIG_CPUSETS_RESTRICTED },
+		{ "top-app",		CONFIG_CPUSETS_TOP_APP },
+		{ "foreground",	CONFIG_CPUSETS_FG },
 	};
 
 	struct cpuset *cs = css_cs(of_css(of));

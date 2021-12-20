46c5d416632451cf8c9b311e300dbdad880a24bc
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2021-12-20 16:40:38 +0700

    AnyKernel: Rename kernel.string to Meow
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel boot install
 # begin properties
 properties() { '
-kernel.string=NEON Kernel by wimbiyoashizkia @ github
+kernel.string=Meow Kernel by wimbiyoashizkia @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=1

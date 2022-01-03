dbf0f648068d78014747fe0b6d0d1bdfbb5fd86b
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-01-03 08:09:00 +0700

    AnyKernel3: Add full support EAS PowerHAL from Google Pixel 2XL
    Import from taimen-user-11-RP1A.201005.004.A1-6934943-release-keys
    
    Changes:
    Import PowerHint
    Import libperfmgr
    Import msm_irqbalance
    Import android.hardware.power@1.3-service.pixel-libperfmgr
    Adapt changes with devices X00T
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- AnyKernel3/patch/eas-powerhal/service.sh
+++ AnyKernel3/patch/eas-powerhal/service.sh
@@ -1,9 +1,35 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
+# Detect whether Unlocked into System
+while $(dumpsys window policy | grep mIsShowing | awk -F= '{print $2}')
+do
+sleep 1
+done
+
 # Optimizations
 stop energy-awareness
 
-#function write() {
-#    echo -n $2 > $1
-#}
+# Kill system's Power HAL
+stop vendor.power-hal-1-2
+stop vendor.power-hal-1-1
+stop vendor.power-hal-1-0
+
+# Init Power HAL
+# Without this hal will waiting for it
+setprop vendor.powerhal.init 1
+
+chown system system /dev/stune/top-app/schedtune.boost
+    chown system system /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+    chown system system /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
+    chown system system /sys/class/kgsl/kgsl-3d0/force_rail_on
+    chown system system /sys/class/kgsl/kgsl-3d0/force_clk_on
+    chown system system /sys/class/kgsl/kgsl-3d0/idle_timer
+    chown system system /sys/class/devfreq/soc:qcom,cpubw/min_freq
+    chown system system /sys/class/devfreq/soc:qcom,cpubw/bw_hwmon/hyst_trigger_count
+    chown system system /sys/class/devfreq/soc:qcom,cpubw/bw_hwmon/hist_memory
+    chown system system /sys/class/devfreq/soc:qcom,cpubw/bw_hwmon/hyst_length
+
+# Start EAS SDM660 Power HAL
+/vendor/bin/hw/android.hardware.power@1.3-service.pixel-libperfmgr
+# Boot ended

4b654a83d25437f706e93ebf7d2c002dbefe2f60
Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>
2022-02-07 01:34:51 +0700

    soc: qcom: msm_performance: Fix invalid usuage of cpumask
    set/get API used for freq updates are using an invalid cpumask
    variables which may potentially lead to stack corruption.
    
    Change-Id: I25fdc5dc25550c39bc81a2efbd55d16679d37e19
    Signed-off-by: Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -54,6 +54,9 @@ static unsigned int aggr_top_load;
 static unsigned int top_load[CLUSTER_MAX];
 static unsigned int curr_cap[CLUSTER_MAX];
 
+static cpumask_var_t limit_mask_min;
+static cpumask_var_t limit_mask_max;
+
 /*******************************sysfs start************************************/
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -62,7 +65,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	const char *cp = buf;
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
-	cpumask_var_t limit_mask;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -72,7 +74,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 		return -EINVAL;
 
 	cp = buf;
-	cpumask_clear(limit_mask);
+	cpumask_clear(limit_mask_min);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
 			return -EINVAL;
@@ -82,7 +84,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 		i_cpu_stats = &per_cpu(msm_perf_cpu_stats, cpu);
 
 		i_cpu_stats->min = val;
-		cpumask_set_cpu(cpu, limit_mask);
+		cpumask_set_cpu(cpu, limit_mask_min);
 
 		cp = strnchr(cp, strlen(cp), ' ');
 		cp++;
@@ -96,7 +98,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	 * in the cluster
 	 */
 	get_online_cpus();
-	for_each_cpu(i, limit_mask) {
+	for_each_cpu(i, limit_mask_min) {
 		i_cpu_stats = &per_cpu(msm_perf_cpu_stats, i);
 
 		if (cpufreq_get_policy(&policy, i))
@@ -106,7 +108,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpufreq_update_policy(i);
 
 		for_each_cpu(j, policy.related_cpus)
-			cpumask_clear_cpu(j, limit_mask);
+			cpumask_clear_cpu(j, limit_mask_min);
 	}
 	put_online_cpus();
 
@@ -140,7 +142,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	const char *cp = buf;
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
-	cpumask_var_t limit_mask;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -150,7 +151,7 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 		return -EINVAL;
 
 	cp = buf;
-	cpumask_clear(limit_mask);
+	cpumask_clear(limit_mask_max);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
 			return -EINVAL;
@@ -160,14 +161,14 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 		i_cpu_stats = &per_cpu(msm_perf_cpu_stats, cpu);
 
 		i_cpu_stats->max = val;
-		cpumask_set_cpu(cpu, limit_mask);
+		cpumask_set_cpu(cpu, limit_mask_max);
 
 		cp = strnchr(cp, strlen(cp), ' ');
 		cp++;
 	}
 
 	get_online_cpus();
-	for_each_cpu(i, limit_mask) {
+	for_each_cpu(i, limit_mask_max) {
 		i_cpu_stats = &per_cpu(msm_perf_cpu_stats, i);
 		if (cpufreq_get_policy(&policy, i))
 			continue;
@@ -176,7 +177,7 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 			cpufreq_update_policy(i);
 
 		for_each_cpu(j, policy.related_cpus)
-			cpumask_clear_cpu(j, limit_mask);
+			cpumask_clear_cpu(j, limit_mask_max);
 	}
 	put_online_cpus();
 
@@ -485,6 +486,14 @@ static int __init msm_performance_init(void)
 
 	for_each_present_cpu(cpu)
 		per_cpu(msm_perf_cpu_stats, cpu).max = UINT_MAX;
+
+	if (!alloc_cpumask_var(&limit_mask_min, GFP_KERNEL))
+		return -ENOMEM;
+
+	if (!alloc_cpumask_var(&limit_mask_max, GFP_KERNEL)) {
+		free_cpumask_var(limit_mask_min);
+		return -ENOMEM;
+	}
 
 	init_events_group();
 	init_notify_group();

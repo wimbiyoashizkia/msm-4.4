1561d76cac55af4a7d10f6ee2eb72dd71f111e6a
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-07-13 18:11:18 +0700

    sched/tune: Avoid unnecessary boost on foreground and top-app tasks
    We restrict iowait boost on kernel side to tasks with prefer_idle only.
    So, to make iowait boost still available for fg and top-app, we have to
    manually mark it as prefer_idle from schedtunu assist.
    
    Following fixups commits cdf9a5d78189248a15a985227cadda7f12bf7248 ("sched/fair: Restrict iowait boost to tasks with prefer_idle").
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -750,10 +750,10 @@ static void write_default_values(struct cgroup_subsys_state *css)
 	static struct st_data st_targets[] = {
 		{ "background",	0, 0 },
 		{ "camera-daemon",	0, 0 },
-		{ "foreground",	0, 0 },
+		{ "foreground",	0, 1 },
 		{ "nnapi-hal",	1, 1 },
 		{ "rt",		0, 0 },
-		{ "top-app",	0, 0 },
+		{ "top-app",	0, 1 },
 	};
 	int i;
 

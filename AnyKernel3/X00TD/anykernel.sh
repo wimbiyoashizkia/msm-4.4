5ebfdce901c848518f56e467581e4883ca1393d7
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-07-28 21:41:00 +0700

    AnyKernel3: Only supported Android 12 or above
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- AnyKernel3/X00TD/anykernel.sh
+++ AnyKernel3/X00TD/anykernel.sh
@@ -15,7 +15,7 @@ device.name2=X00TD
 device.name3=ASUS_X00T
 device.name4=ASUS_X00TD
 device.name5=ASUS_X00TDA
-supported.versions=
+supported.versions=12
 supported.patchlevels=
 '; } # end properties
 

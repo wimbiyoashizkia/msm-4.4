e8a1765b42454d68eb6978e29b7d27ca39270f57
Yaroslav Furman <yaro330@gmail.com>
2022-08-10 19:56:27 +0700

    input/touchscreen: nt36xxx: Carefully optimize coordinates reporting
    - Remove an unneeded while loop, CTP_I2C_READ has it's own error checking.
    - Extend XFER_ERROR slightly, sunc input anyway even if we encounter an
      error to avoid issues with touchscreen randomly dying.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.c
+++ drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.c
@@ -1313,14 +1313,10 @@ static void nvt_ts_work_func(struct work_struct *work)
 	}
 // Huaqin add for ZQL1650-1072. by zhengwu.lu. at 2018/04/23  end
 #if WAKEUP_GESTURE
-	if (bTouchIsAwake == 0) {
+	if (unlikely(bTouchIsAwake == 0)) {
 		input_id = (uint8_t)(point_data[1] >> 3);
 		nvt_ts_wakeup_gesture_report(input_id);
-// Huaqin add for ctp lose efficacy by zhengwu.lu. at 2018/04/18 For Platform start
-		//enable_irq(ts->client->irq);
-		nvt_irq_enable();
-// Huaqin add for ctp lose efficacy by zhengwu.lu. at 2018/04/18 For Platform end
-		mutex_unlock(&ts->lock);
+		goto XFER_ERROR;
 		return;
 	}
 #endif
@@ -1414,9 +1410,9 @@ static void nvt_ts_work_func(struct work_struct *work)
 	}
 #endif
 
+XFER_ERROR:
 	input_sync(ts->input_dev);
 
-XFER_ERROR:
 // Huaqin add for ctp lose efficacy by zhengwu.lu. at 2018/04/18 For Platform start
 	//enable_irq(ts->client->irq);
 	nvt_irq_enable();

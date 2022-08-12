1397d1a03895983077cc36798ff7e06533ecb972
LibXZR <xzr467706992@163.com>
2022-08-12 17:15:11 +0700

    kernel: kthread: Split PF_PERF_CRITICAL into 2 standalone flags
    This is needed because userspace sometimes overrides the affinity of
    specific tasks e.g. hwcomposer. Kernel can't stop it when the new mask
    is in cpu_perf_mask and cpu_lp_mask. Split the general
    flag into specific one to fix it.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1705,17 +1705,17 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN,
 					   strlen(HWCOMPOSER_BIN)))) {
-			current->flags |= PF_PERF_CRITICAL;
+			current->pf_flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name,
 					   SURFACEFLINGER_BIN,
 					   strlen(SURFACEFLINGER_BIN)))) {
-			current->flags |= PF_PERF_CRITICAL;
+			current->pf_flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name,
 					   FINGERPRINT_BIN,
 					   strlen(FINGERPRINT_BIN)))) {
-			current->flags |= PF_PERF_CRITICAL;
+			current->pf_flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 			fp_daemon = current;
 		}

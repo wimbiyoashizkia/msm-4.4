5b88ffec7081eb0229c5c87234341a9b4b25202a
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-08-17 06:59:55 +0700

    input/touchscreen: nt36xxx: Move sched_priority to nt36xxx.c
    warning:
    ../drivers/input/touchscreen/NVT_Touch_X01BD/nt36xxx.h:133:27: warning: 'param' defined but not used [-Wunused-variable]
      133 | static struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
          |
    
    ../drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.h:112:27: warning: 'param' defined but not used [-Wunused-variable]
      112 | static struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
          |
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.h
+++ drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.h
@@ -130,8 +130,6 @@ struct nvt_ts_mem_map {
 	uint32_t RW_FLASH_DATA_ADDR;
 };
 
-static struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
-
 struct nvt_ts_data {
 	struct i2c_client *client;
 	struct input_dev *input_dev;

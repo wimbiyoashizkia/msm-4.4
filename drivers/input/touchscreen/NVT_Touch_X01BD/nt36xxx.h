b4cb88cd1cd085df58c2d948e5857dd288094fa1
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-08-18 20:40:30 +0700

    input/touchscreen: nt36xxx: Use FIFO kthread for work
    Serialize worker thread with a dedicated kworker and schedule it over rt FIFO.
    
    pref:
    * tytydraco/android_kernel_xiaomi_jasmine@b0219a4
    * AOSPA/android_kernel_oneplus_msm8996@2d0c33f
    
    Test: Device boots, touchscreen works perfectly.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/input/touchscreen/NVT_Touch_X01BD/nt36xxx.h
+++ drivers/input/touchscreen/NVT_Touch_X01BD/nt36xxx.h
@@ -23,6 +23,8 @@
 #include <linux/regulator/consumer.h>
 /* #include <linux/debugfs.h> */
 /* Huaqin add by zhangxiude for ITO test end */
+#include <linux/kthread.h>
+#include <linux/sched/rt.h>
 
 #include <linux/i2c.h>
 #include <linux/input.h>
@@ -110,7 +112,7 @@ extern const uint16_t gesture_key_array[];
 struct nvt_ts_data {
 	struct i2c_client *client;
 	struct input_dev *input_dev;
-	struct work_struct nvt_work;
+	struct kthread_work nvt_work;
 	struct delayed_work nvt_fwu_work;
 	uint16_t addr;
 	int8_t phys[32];

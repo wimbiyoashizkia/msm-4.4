c8864cb86cfcd43f8436480e4f7c83271c9a3ff2
Andrzej Perczak <linux@andrzejperczak.com>
2022-08-19 09:58:24 +0700

    alarmtimer: Minimize wakeup time
    Alarmtimer sets its wakeup timeout to 2s no matter the actual time
    to nearest timer expiration. This can cause device to be awake for more
    than needed.
    
    To fix this set wakeup timeout to min + 1 ms for safety margin.
    
    Tests revealed that average timer expiration is 1150ms in the future
    which suggests there is a room avilable to minimize wakeup times.
    Before this change device would enter sleep not earlier than 2s after
    alarmtimer suspend error (-EBUSY). With this change average suspend
    after alarmtimer suspend error time went down to 1.5s with a minimum of
    0.248ms (after filtering results higher than 2.6s).
    
    This should lead to noticeable power savings as Android uses alarmtimer
    quite frequently.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -285,7 +285,7 @@ static int alarmtimer_suspend(struct device *dev)
 		return 0;
 
 	if (ktime_to_ns(min) < NSEC_PER_SEC / 2)
-		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
+		__pm_wakeup_event(ws, ktime_to_ms(min) + 1);
 
 	/* Setup a timer to fire that far in the future */
 	rtc_timer_cancel(rtc, &rtctimer);
@@ -344,7 +344,7 @@ static int alarmtimer_suspend(struct device *dev)
 		return 0;
 
 	if (ktime_to_ns(min) < NSEC_PER_SEC / 2)
-		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
+		__pm_wakeup_event(ws, ktime_to_ms(min) + 1);
 
 	/* Setup an rtc timer to fire that far in the future */
 	rtc_timer_cancel(rtc, &rtctimer);

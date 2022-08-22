8cc24d516044d1ea1db77ec1a1e1b65f6c875ec9
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-08-22 21:40:11 +0700

    kernel: Reduce period of time top-app boost
    Top-app transitions are more so DDR-bound than memory bound, so boost
    the DDR to the max for a short period of time.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2981,7 +2981,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
             !strcmp(of->kn->parent->name, "top-app") &&
             task_is_zygote(tsk->parent) &&
             time_before(jiffies, last_input_time + msecs_to_jiffies(75))) {
-            devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+            devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 256);
         }
 
 	put_task_struct(tsk);

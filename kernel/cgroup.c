84c9b9becead6c5af92f32553400588365e34a66
kdrag0n <dragon@khronodragon.com>
2022-08-22 21:36:00 +0700

    kernel: Reduce app launch boost timeout to 75 ms
    If the device takes longer than 75 ms to fork zygote after the user taps
    an app icon, it has bigger issues.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2980,7 +2980,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
         if (!ret && !threadgroup && 
             !strcmp(of->kn->parent->name, "top-app") &&
             task_is_zygote(tsk->parent) &&
-            time_before(jiffies, last_input_time + msecs_to_jiffies(150))) {
+            time_before(jiffies, last_input_time + msecs_to_jiffies(75))) {
             devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
         }
 

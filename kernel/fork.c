f14a6cbbe112e4a685321d796dc308c63e823a67
kdrag0n <dragon@khronodragon.com>
2022-08-22 21:36:00 +0700

    kernel: Reduce app launch boost timeout to 75 ms
    If the device takes longer than 75 ms to fork zygote after the user taps
    an app icon, it has bigger issues.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1915,7 +1915,7 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current) &&
-		time_before(jiffies, last_input_time + msecs_to_jiffies(150))) {
+		time_before(jiffies, last_input_time + msecs_to_jiffies(75))) {
 		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}

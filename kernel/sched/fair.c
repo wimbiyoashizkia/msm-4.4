wimbiyoashizkia/msm-4.4

commit d201a5ee48f331a4e52c87fef70050fe4b80723e

sched: Add PELT cmdline arg
The PELT half-life is currently hard-coded to 8ms.

Create cmdline arg to enable switching to a PELT half-life.

Bug: 177593580
Change-Id: I9f8cfc3d9554a500eec0f6a1b161f4155c296b4d
Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>
Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

kernel/sched/fair.c
@@ -2678,6 +2678,48 @@ int core_ctl_set_boost(bool boost)
 EXPORT_SYMBOL(core_ctl_set_boost);
 
 #ifdef CONFIG_SMP
+int pelt_load_avg_period = PELT32_LOAD_AVG_PERIOD;
+int pelt_load_avg_max = PELT32_LOAD_AVG_MAX;
+u32 *pelt_runnable_avg_yN_inv = pelt32_runnable_avg_yN_inv;
+
+static int __init set_pelt(char *str)
+{
+	int rc, num;
+
+	rc = kstrtoint(str, 0, &num);
+	if (rc) {
+		pr_err("%s: kstrtoint failed. rc=%d\n", __func__, rc);
+		return 0;
+	}
+
+	switch (num) {
+	case PELT8_LOAD_AVG_PERIOD:
+		pelt_load_avg_period = PELT8_LOAD_AVG_PERIOD;
+		pelt_load_avg_max = PELT8_LOAD_AVG_MAX;
+		pelt_runnable_avg_yN_inv = pelt8_runnable_avg_yN_inv;
+		pr_info("PELT half life is set to %dms\n", num);
+		break;
+	case PELT16_LOAD_AVG_PERIOD:
+		pelt_load_avg_period = PELT16_LOAD_AVG_PERIOD;
+		pelt_load_avg_max = PELT16_LOAD_AVG_MAX;
+		pelt_runnable_avg_yN_inv = pelt16_runnable_avg_yN_inv;
+		pr_info("PELT half life is set to %dms\n", num);
+		break;
+	case PELT32_LOAD_AVG_PERIOD:
+		pelt_load_avg_period = PELT32_LOAD_AVG_PERIOD;
+		pelt_load_avg_max = PELT32_LOAD_AVG_MAX;
+		pelt_runnable_avg_yN_inv = pelt32_runnable_avg_yN_inv;
+		pr_info("PELT half life is set to %dms\n", num);
+		break;
+	default:
+		pr_err("Default PELT half life is 32ms\n");
+	}
+
+	return 0;
+}
+
+early_param("pelt", set_pelt);
+
 /*
  * Approximate:
  *   val * y^n,    where y^32 ~= 0.5 (~1 scheduling period)
@@ -2704,7 +2746,7 @@ static __always_inline u64 decay_load(u64 val, u64 n)
 		local_n %= LOAD_AVG_PERIOD;
 	}
 
-	val = mul_u64_u32_shr(val, runnable_avg_yN_inv[local_n], 32);
+	val = mul_u64_u32_shr(val, pelt_runnable_avg_yN_inv[local_n], 32);
 	return val;
 }
 

kernel/sched/sched-pelt.h
@@ -1,9 +1,37 @@
 /* Generated by Documentation/scheduler/sched-pelt; do not modify. */
 
-static const u32 runnable_avg_yN_inv[] = {
-	0xffffffff, 0xeac0c6e6, 0xd744fcc9, 0xc5672a10,
-	0xb504f333, 0xa5fed6a9, 0x9837f050, 0x8b95c1e3,
+static u32 pelt32_runnable_avg_yN_inv[] = {
+	0xffffffff, 0xfa83b2da, 0xf5257d14, 0xefe4b99a, 0xeac0c6e6, 0xe5b906e6,
+	0xe0ccdeeb, 0xdbfbb796, 0xd744fcc9, 0xd2a81d91, 0xce248c14, 0xc9b9bd85,
+	0xc5672a10, 0xc12c4cc9, 0xbd08a39e, 0xb8fbaf46, 0xb504f333, 0xb123f581,
+	0xad583ee9, 0xa9a15ab4, 0xa5fed6a9, 0xa2704302, 0x9ef5325f, 0x9b8d39b9,
+	0x9837f050, 0x94f4efa8, 0x91c3d373, 0x8ea4398a, 0x8b95c1e3, 0x88980e80,
+	0x85aac367, 0x82cd8698,
 };
 
-#define LOAD_AVG_PERIOD 8
-#define LOAD_AVG_MAX 12326
+#define PELT32_LOAD_AVG_PERIOD 32
+#define PELT32_LOAD_AVG_MAX 47742
+
+static const u32 pelt16_runnable_avg_yN_inv[] = {
+	0xffffffff, 0xf5257d14, 0xeac0c6e6, 0xe0ccdeeb, 0xd744fcc9, 0xce248c14,
+	0xc5672a10, 0xbd08a39e, 0xb504f333, 0xad583ee9, 0xa5fed6a9, 0x9ef5325f,
+	0x9837f050, 0x91c3d373, 0x8b95c1e3, 0x85aac367,
+};
+
+#define PELT16_LOAD_AVG_PERIOD 16
+#define PELT16_LOAD_AVG_MAX 24130
+
+static u32 pelt8_runnable_avg_yN_inv[] = {
+	0xffffffff, 0xeac0c6e6, 0xd744fcc9, 0xc5672a10, 0xb504f333, 0xa5fed6a9,
+	0x9837f050, 0x8b95c1e3,
+};
+
+#define PELT8_LOAD_AVG_PERIOD 8
+#define PELT8_LOAD_AVG_MAX 12336
+
+extern u32 *pelt_runnable_avg_yN_inv;
+extern int pelt_load_avg_period;
+extern int pelt_load_avg_max;
+
+#define LOAD_AVG_PERIOD pelt_load_avg_period
+#define LOAD_AVG_MAX pelt_load_avg_max

commit 66932786c56a9330080999eb066ea650cc655c70

sched: Fix the PELT arrays
Commit d201a5ee48f3 ("sched: Add PELT cmdline arg") removed the
'__maybe_unused' and 'const' qualifiers from the declaration of the PELT
arrays for no apparent reason.

Re-introduce both of them to silence the 'Maybe unused' warnings and
align as much as possible with upstream.

Bug: 177593580
Fixes: d201a5ee48f3 ("sched: Add PELT cmdline arg")
Signed-off-by: Quentin Perret <qperret@google.com>
Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

kernel/sched/fair.c
@@ -2680,7 +2680,7 @@ EXPORT_SYMBOL(core_ctl_set_boost);
 #ifdef CONFIG_SMP
 int pelt_load_avg_period = PELT32_LOAD_AVG_PERIOD;
 int pelt_load_avg_max = PELT32_LOAD_AVG_MAX;
-u32 *pelt_runnable_avg_yN_inv = pelt32_runnable_avg_yN_inv;
+const u32 *pelt_runnable_avg_yN_inv = pelt32_runnable_avg_yN_inv;
 
 int get_pelt_halflife(void)
 {

kernel/sched/sched-pelt.h
@@ -1,6 +1,6 @@
 /* Generated by Documentation/scheduler/sched-pelt; do not modify. */
 
-static u32 pelt32_runnable_avg_yN_inv[] = {
+static const u32 pelt32_runnable_avg_yN_inv[] __maybe_unused = {
 	0xffffffff, 0xfa83b2da, 0xf5257d14, 0xefe4b99a, 0xeac0c6e6, 0xe5b906e6,
 	0xe0ccdeeb, 0xdbfbb796, 0xd744fcc9, 0xd2a81d91, 0xce248c14, 0xc9b9bd85,
 	0xc5672a10, 0xc12c4cc9, 0xbd08a39e, 0xb8fbaf46, 0xb504f333, 0xb123f581,
@@ -12,7 +12,7 @@ static u32 pelt32_runnable_avg_yN_inv[] = {
 #define PELT32_LOAD_AVG_PERIOD 32
 #define PELT32_LOAD_AVG_MAX 47742
 
-static const u32 pelt16_runnable_avg_yN_inv[] = {
+static const u32 pelt16_runnable_avg_yN_inv[] __maybe_unused= {
 	0xffffffff, 0xf5257d14, 0xeac0c6e6, 0xe0ccdeeb, 0xd744fcc9, 0xce248c14,
 	0xc5672a10, 0xbd08a39e, 0xb504f333, 0xad583ee9, 0xa5fed6a9, 0x9ef5325f,
 	0x9837f050, 0x91c3d373, 0x8b95c1e3, 0x85aac367,
@@ -21,7 +21,7 @@ static const u32 pelt16_runnable_avg_yN_inv[] = {
 #define PELT16_LOAD_AVG_PERIOD 16
 #define PELT16_LOAD_AVG_MAX 24130
 
-static u32 pelt8_runnable_avg_yN_inv[] = {
+static const u32 pelt8_runnable_avg_yN_inv[] __maybe_unused= {
 	0xffffffff, 0xeac0c6e6, 0xd744fcc9, 0xc5672a10, 0xb504f333, 0xa5fed6a9,
 	0x9837f050, 0x8b95c1e3,
 };
@@ -29,7 +29,7 @@ static u32 pelt8_runnable_avg_yN_inv[] = {
 #define PELT8_LOAD_AVG_PERIOD 8
 #define PELT8_LOAD_AVG_MAX 12336
 
-extern u32 *pelt_runnable_avg_yN_inv;
+extern const u32 *pelt_runnable_avg_yN_inv;
 extern int pelt_load_avg_period;
 extern int pelt_load_avg_max;
 
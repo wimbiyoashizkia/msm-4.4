554cf4b16b5d4dce0dc4281e1b6b83c92b1f4b51
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-10-02 06:18:24 +0700

    lowmemorykiller: Remove sysfs param for adaptive LMK
    Adaptive LMK has been disabled here, but in
    init.qcom.post_boot.sh script it is re-enabled,
    so remove the sysfs param to keep adaptive LMK disabled.
    
    Note: This results in userspace not being able
    to enable/disable adaptive LMK.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -241,8 +241,6 @@ module_param_named(adj_max_shift, adj_max_shift, short,
 
 /* User knob to enable/disable adaptive lmk feature */
 static int enable_adaptive_lmk = 0;
-module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
-		   S_IRUGO | S_IWUSR);
 
 /*
  * This parameter controls the behaviour of LMK when vmpressure is in

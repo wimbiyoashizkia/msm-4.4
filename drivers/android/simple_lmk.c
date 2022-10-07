6d0737f01ad57bae50debb73e543ab7950f16e33
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-10-07 20:30:11 +0700

    simple_lmk: Rewrite to improve clarity and performance
    Inspired from lowmemorykiller and based on oom_score_adj.
    This is make oom_score_adj value as secondary condition it
    will depends on process priority.
    
    That way can drop a lot of variables on struct, therefore saving
    memory a bit kernel will calcuate the scores now. This is no added
    proper method of checking if app process and also fix a unintended
    deadlock, so it will back to depends on process priority. Else the
    oom_score_adj measured by kernel with be passed to kill function,
    it will kill according to the scores.
    
    But, now simple lmk will check detection foregorund logic from /dev/ion.
    Because on /dev/ion opened fast (lower fd number) and core memory management
    device for 4.4 kernel, and it's easy to make this check for detect foreground
    logic also check PPID too for dead threads.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -1,26 +1,29 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2019-2021 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2022 Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>
  */
 
 #define pr_fmt(fmt) "simple_lmk: " fmt
 
+#include <linux/dcache.h>
+#include <linux/delay.h>
+#include <linux/fdtable.h>
 #include <linux/freezer.h>
-#include <linux/kthread.h>
+#include <linux/fs.h>
+#include <linux/fs_struct.h>
 #include <linux/mm.h>
 #include <linux/moduleparam.h>
 #include <linux/oom.h>
-#include <linux/sort.h>
-#include <linux/vmpressure.h>
-
-/* The minimum number of pages to free per reclaim */
-#define MIN_FREE_PAGES (CONFIG_ANDROID_SIMPLE_LMK_MINFREE * SZ_1M / PAGE_SIZE)
+#include <linux/pid_namespace.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/swap.h>
+#include <linux/vmalloc.h>
 
 /* Kill up to this many victims per reclaim */
-#define MAX_VICTIMS 1024
-
-/* Timeout in jiffies for each reclaim */
-#define RECLAIM_EXPIRES msecs_to_jiffies(CONFIG_ANDROID_SIMPLE_LMK_TIMEOUT_MSEC)
+#define MAX_VICTIMS 32
+#define MAX_FOREGROUND 18
 
 struct victim_info {
 	struct task_struct *tsk;
@@ -30,28 +33,17 @@ struct victim_info {
 
 static struct victim_info victims[MAX_VICTIMS] __cacheline_aligned_in_smp;
 static struct task_struct *task_bucket[SHRT_MAX + 1] __cacheline_aligned;
-static DECLARE_WAIT_QUEUE_HEAD(oom_waitq);
-static DECLARE_COMPLETION(reclaim_done);
-static __cacheline_aligned_in_smp DEFINE_RWLOCK(mm_free_lock);
 static int nr_victims;
-static atomic_t needs_reclaim = ATOMIC_INIT(0);
-static atomic_t nr_killed = ATOMIC_INIT(0);
 
-static int victim_cmp(const void *lhs_ptr, const void *rhs_ptr)
-{
-	const struct victim_info *lhs = (typeof(lhs))lhs_ptr;
-	const struct victim_info *rhs = (typeof(rhs))rhs_ptr;
+static int foreground_start = 87;
+module_param(foreground_start, int, 0664);
 
-	return rhs->size - lhs->size;
-}
+static int background_start = 83;
+module_param(background_start, int, 0664);
 
-static void victim_swap(void *lhs_ptr, void *rhs_ptr, int size)
-{
-	struct victim_info *lhs = (typeof(lhs))lhs_ptr;
-	struct victim_info *rhs = (typeof(rhs))rhs_ptr;
-
-	swap(*lhs, *rhs);
-}
+static void simple_lmk_reclaim_work(struct work_struct *data);
+static struct workqueue_struct *kill_work_queue;
+static DECLARE_DELAYED_WORK(kill_task_work, simple_lmk_reclaim_work);
 
 static unsigned long get_total_mm_pages(struct mm_struct *mm)
 {
@@ -138,15 +130,8 @@ static unsigned long find_victims(int *vindex)
 		if (*vindex == old_vindex)
 			continue;
 
-		/*
-		 * Sort the victims in descending order of size to prioritize
-		 * killing the larger ones first.
-		 */
-		sort(&victims[old_vindex], *vindex - old_vindex,
-		     sizeof(*victims), victim_cmp, victim_swap);
-
 		/* Stop when we are out of space or have enough pages found */
-		if (*vindex == MAX_VICTIMS || pages_found >= MIN_FREE_PAGES) {
+		if (*vindex == MAX_VICTIMS) {
 			/* Zero out any remaining buckets we didn't touch */
 			if (i > min_adj)
 				memset(&task_bucket[min_adj], 0,
@@ -159,35 +144,266 @@ static unsigned long find_victims(int *vindex)
 	return pages_found;
 }
 
-static int process_victims(int vlen)
+static void kill_task(struct task_struct *vtsk)
 {
-	unsigned long pages_found = 0;
-	int i, nr_to_kill = 0;
+	static const struct sched_param sched_zero_prio;
+	struct task_struct *t;
 
-	/*
-	 * Calculate the number of tasks that need to be killed and quickly
-	 * release the references to those that'll live.
-	 */
-	for (i = 0; i < vlen; i++) {
-		struct victim_info *victim = &victims[i];
-		struct task_struct *vtsk = victim->tsk;
+	/* Accelerate the victim's death by forcing the kill signal */
+	group_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk);
 
-		/* The victim's mm lock is taken in find_victims; release it */
-		if (pages_found >= MIN_FREE_PAGES) {
-			task_unlock(vtsk);
-		} else {
-			pages_found += victim->size;
-			nr_to_kill++;
+	/* Mark the thread group dead so that other kernel code knows */
+	rcu_read_lock();
+	for_each_thread(vtsk, t)
+		set_tsk_thread_flag(t, TIF_MEMDIE);
+	rcu_read_unlock();
+
+	/* Elevate the victim to SCHED_RR with zero RT priority */
+	sched_setscheduler_nocheck(vtsk, SCHED_RR, &sched_zero_prio);
+
+	/* Allow the victim to run on any CPU. This won't schedule. */
+	set_cpus_allowed_ptr(vtsk, cpu_all_mask);
+
+	/* Signals can't wake frozen tasks; only a thaw operation can */
+	__thaw_task(vtsk);
+
+	/* Finally release the victim's task lock acquired earlier */
+	task_unlock(vtsk);
+}
+
+struct process_data {
+	int pid;
+	int uid;
+	unsigned long score;
+};
+
+static struct process_data processes[MAX_VICTIMS];
+static int foreground[MAX_FOREGROUND];
+
+static int get_mm_usage(void)
+{
+	static struct sysinfo info;
+	static long cached;
+
+	si_meminfo(&info);
+
+	cached = global_page_state(NR_FILE_PAGES) - total_swapcache_pages() - info.bufferram;
+	if (cached < 0)
+		cached = 0;
+
+	return 100 - ((info.freeram + info.bufferram + cached) / (info.totalram / 100));
+}
+
+static void put_new_foreground(struct task_struct *tsk)
+{
+	static int index = 0, i, pid_to_add;
+	pid_to_add = tsk->pid;
+
+	rcu_read_lock();
+	if (task_uid(rcu_dereference(tsk->real_parent)).val != 0) {
+		pid_to_add = rcu_dereference(tsk->real_parent)->pid;
+	}
+	rcu_read_unlock();
+
+	for (i = 0; i < MAX_FOREGROUND; i++) {
+		if (foreground[i] == pid_to_add) {
+			pr_info("Abort, pid %d already on list\n", pid_to_add);
+			return;
 		}
 	}
 
-	return nr_to_kill;
+	index++;
+	if (index == MAX_FOREGROUND) {
+		int tmp[MAX_FOREGROUND] = {0}, count = 0;
+		struct task_struct *task;
+
+		for (i = 0; i < MAX_FOREGROUND; i++) {
+			rcu_read_lock();
+			task = get_pid_task(find_get_pid(foreground[i]), PIDTYPE_PID);
+			rcu_read_unlock();
+
+			if (!task)
+				continue;
+
+			tmp[count] = foreground[i];
+			count++;
+		}
+
+		for (i = 0; i < MAX_FOREGROUND; i++) {
+			foreground[i] = i >= count ? -1 : tmp[i];
+		}
+		index = count;
+	}
+
+	pr_info("Adding pid %d to list\n", pid_to_add);
+
+	foreground[index] = pid_to_add;
+}
+
+static bool check_fd_for_ion(struct task_struct *tsk)
+{
+	struct files_struct *current_files;
+	struct fdtable *files_table;
+	int i = 0;
+	char *cwd;
+	char *buf = (char *)kmalloc(PAGE_SIZE, GFP_KERNEL | __GFP_NORETRY | __GFP_NOWARN);
+
+	 if (!buf) {
+	 	pr_err("Failed to alloc buffer.\n");
+	 	return false;
+	 }
+
+	/*
+	 * The task is dead here, but we need to
+	 * skip killing this task, therefore return true.
+	 */
+	if (!pid_alive(tsk)) {
+		pr_err("Task is dead.\n");
+		kfree(buf);
+		return true;
+	}
+
+	current_files = tsk->files;
+	if (!current_files) {
+		pr_err("Error task->files is NULL.\n");
+		kfree(buf);
+		return false;
+	}
+
+	spin_lock(&current_files->file_lock);
+	files_table = files_fdtable(current_files);
+	if (!files_table) {
+		pr_err("The files_fdtable return value is NULL.\n");
+		kfree(buf);
+		spin_unlock(&current_files->file_lock);
+		return false;
+	}
+
+	while (i <= files_table->max_fds) {
+		struct file *fd_file = fcheck_files(current_files, i);
+
+		if (!fd_file) {
+			i++;
+			continue;
+		}
+
+		cwd = d_path(&fd_file->f_path, buf, PAGE_SIZE);
+		if (!cwd) {
+			i++;
+			continue;
+		}
+
+		if (strstr(cwd, "/dev/ion")) {
+			pr_info("Check has /dev/ion open as fd %d\n", i);
+			put_new_foreground(tsk);
+			kfree(buf);
+			spin_unlock(&current_files->file_lock);
+			return true;
+		}
+		i++;
+	}
+	kfree(buf);
+	spin_unlock(&current_files->file_lock);
+
+	return false;
 }
 
 static void scan_and_kill(void)
 {
-	int i, nr_to_kill, nr_found = 0;
+	int nr_found = 0, i;
 	unsigned long pages_found;
+	struct task_struct *tsk;
+
+	for (i = 0; i < MAX_VICTIMS; i++) {
+		tsk = get_pid_task(find_get_pid(processes[i].pid), PIDTYPE_PID);
+		if (tsk && pid_alive(tsk)) {
+			struct task_struct *vtsk;
+			bool is_foreground;
+			int ppid, k, usage;
+
+			task_lock(tsk);
+			is_foreground = check_fd_for_ion(tsk);
+			for (k = 0; k < MAX_FOREGROUND; k++) {
+				if (foreground[k] == tsk->pid) {
+					is_foreground = true;
+					goto final_check;
+				}
+
+				vtsk = get_pid_task(find_get_pid(foreground[k]), PIDTYPE_PID);
+				if (vtsk && pid_alive(vtsk)) {
+					if (processes[k].uid == task_uid(vtsk).val) {
+						is_foreground = true;
+						goto final_check;
+					}
+
+					if (task_uid(rcu_dereference(tsk->real_parent)).val == task_uid(vtsk).val) {
+						pr_info("Skip, pid: %d, due to task parent UID\n", tsk->pid);
+						is_foreground = true;
+						goto final_check;
+					}
+				}
+			}
+
+			rcu_read_lock();
+			ppid = pid_alive(tsk) ? task_tgid_nr_ns(rcu_dereference(tsk->real_parent),
+				task_active_pid_ns(tsk)) : 0;
+
+			if (check_fd_for_ion(rcu_dereference(tsk->real_parent))) {
+				pr_info("Skip, pid: %d, found parent comm: %s, pid: %d\n", tsk->pid,
+					rcu_dereference(tsk->real_parent)->comm,
+					rcu_dereference(tsk->real_parent)->pid);
+				is_foreground = true;
+				rcu_read_unlock();
+				goto final_check;
+			}
+
+			/* Android WebView zygote process UID */
+			if (task_uid(rcu_dereference(tsk->real_parent)).val == 1053) {
+				pr_info("Skip, pid: %d, instance of Android WebView zygote\n", tsk->pid);
+				is_foreground = true;
+				rcu_read_unlock();
+				goto final_check;
+			}
+
+			if (processes[i].score <= 350)
+				is_foreground = true;
+
+			rcu_read_unlock();
+
+final_check:
+			/* It lost when parent process */
+			if (ppid == 1)
+				is_foreground = false;
+
+			usage = get_mm_usage();
+			if (usage < background_start) {
+				if (is_foreground || strcmp(tsk->comm, "su") == 0) {
+					task_unlock(tsk);
+					continue;
+				}
+			} else if (background_start <= usage && usage < foreground_start) {
+				if (tsk->signal->oom_score_adj == 0 || strcmp(tsk->comm, "su") == 0) {
+					task_unlock(tsk);
+					continue;
+				}
+			}
+
+			pr_info("Killing task, pid: %d, system_mm_usage: %d\n", tsk->pid,
+				get_mm_usage());
+
+			/* Call function of speed up the death process */
+			kill_task(tsk);
+
+			usleep_range(550000, 600000);
+		}
+	}
+
+	/* Reset the collected data */
+	for (i = 0; i < MAX_VICTIMS; i++) {
+		processes[i].pid = -1;
+		processes[i].uid = -1;
+		processes[i].score = -1;
+	}
 
 	/* Populate the victims array with tasks sorted by adj and then size */
 	pages_found = find_victims(&nr_found);
@@ -196,139 +412,50 @@ static void scan_and_kill(void)
 		return;
 	}
 
-	/* Minimize the number of victims if we found more pages than needed */
-	if (pages_found > MIN_FREE_PAGES) {
-		/* First round of processing to weed out unneeded victims */
-		nr_to_kill = process_victims(nr_found);
-
-		/*
-		 * Try to kill as few of the chosen victims as possible by
-		 * sorting the chosen victims by size, which means larger
-		 * victims that have a lower adj can be killed in place of
-		 * smaller victims with a high adj.
-		 */
-		sort(victims, nr_to_kill, sizeof(*victims), victim_cmp,
-		     victim_swap);
-
-		/* Second round of processing to finally select the victims */
-		nr_to_kill = process_victims(nr_to_kill);
-	} else {
-		/* Too few pages found, so all the victims need to be killed */
-		nr_to_kill = nr_found;
-	}
-
-	/* Store the final number of victims for simple_lmk_mm_freed() */
-	write_lock(&mm_free_lock);
-	nr_victims = nr_to_kill;
-	write_unlock(&mm_free_lock);
+	/* Store the final number of victims */
+	nr_victims = nr_found;
 
 	/* Kill the victims */
-	for (i = 0; i < nr_to_kill; i++) {
-		static const struct sched_param sched_zero_prio;
+	for (i = 0; i < nr_victims; i++) {
 		struct victim_info *victim = &victims[i];
-		struct task_struct *t, *vtsk = victim->tsk;
+		struct task_struct *vtsk = victim->tsk;
+		unsigned long totalpages = totalram_pages() + total_swap_pages;
 
-		pr_info("Killing %s with adj %d to free %lu KiB\n", vtsk->comm,
-			vtsk->signal->oom_score_adj,
-			victim->size << (PAGE_SHIFT - 10));
-
-		/* Accelerate the victim's death by forcing the kill signal */
-		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk, true);
-
-		/* Mark the thread group dead so that other kernel code knows */
-		rcu_read_lock();
-		for_each_thread(vtsk, t)
-			set_tsk_thread_flag(t, TIF_MEMDIE);
-		rcu_read_unlock();
-
-		/* Elevate the victim to SCHED_RR with zero RT priority */
-		sched_setscheduler_nocheck(vtsk, SCHED_RR, &sched_zero_prio);
-
-		/* Allow the victim to run on any CPU. This won't schedule. */
-		set_cpus_allowed_ptr(vtsk, cpu_all_mask);
-
-		/* Signals can't wake frozen tasks; only a thaw operation can */
-		__thaw_task(vtsk);
-
-		/* Finally release the victim's task lock acquired earlier */
+		processes[i].pid = vtsk->pid;
+		task_unlock(vtsk);
+		processes[i].score = oom_badness(vtsk, NULL, NULL, totalpages) * 1000 / totalpages;
+		task_lock(vtsk);
+		processes[i].uid = task_uid(vtsk).val;
+		pr_info("Process pid: %d, uid: %d\n", processes[i].pid,
+			processes[i].uid);
 		task_unlock(vtsk);
 	}
-
-	/* Wait until all the victims die or until the timeout is reached */
-	if (!wait_for_completion_timeout(&reclaim_done, RECLAIM_EXPIRES))
-		pr_info("Timeout hit waiting for victims to die, proceeding\n");
-
-	/* Clean up for future reclaim invocations */
-	write_lock(&mm_free_lock);
-	reinit_completion(&reclaim_done);
 	nr_victims = 0;
-	nr_killed = (atomic_t)ATOMIC_INIT(0);
-	write_unlock(&mm_free_lock);
 }
 
-static int simple_lmk_reclaim_thread(void *data)
+static void simple_lmk_reclaim_work(struct work_struct *data)
 {
-	static const struct sched_param sched_max_rt_prio = {
-		.sched_priority = MAX_RT_PRIO - 1
-	};
-
-	sched_setscheduler_nocheck(current, SCHED_FIFO, &sched_max_rt_prio);
-	set_freezable();
-
-	while (1) {
-		wait_event_freezable(oom_waitq, atomic_read(&needs_reclaim));
-		scan_and_kill();
-		atomic_set_release(&needs_reclaim, 0);
-	}
-
-	return 0;
+	scan_and_kill();
+	queue_delayed_work(kill_work_queue, &kill_task_work, 5000);
 }
-
-void simple_lmk_mm_freed(struct mm_struct *mm)
-{
-	int i;
-
-	/* Nothing to do when reclaim is starting or ending */
-	if (!read_trylock(&mm_free_lock))
-		return;
-
-	for (i = 0; i < nr_victims; i++) {
-		if (victims[i].mm == mm) {
-			victims[i].mm = NULL;
-			if (atomic_inc_return_relaxed(&nr_killed) == nr_victims)
-				complete(&reclaim_done);
-			break;
-		}
-	}
-	read_unlock(&mm_free_lock);
-}
-
-static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
-				    unsigned long pressure, void *data)
-{
-	if (pressure == 100 && !atomic_cmpxchg_acquire(&needs_reclaim, 0, 1))
-		wake_up(&oom_waitq);
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block vmpressure_notif = {
-	.notifier_call = simple_lmk_vmpressure_cb,
-	.priority = INT_MAX
-};
 
 /* Initialize Simple LMK when lmkd in Android writes to the minfree parameter */
 static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 {
-	static atomic_t init_done = ATOMIC_INIT(0);
-	struct task_struct *thread;
+	int i;
 
-	if (!atomic_cmpxchg(&init_done, 0, 1)) {
-		thread = kthread_run(simple_lmk_reclaim_thread, NULL,
-				     "simple_lmkd");
-		BUG_ON(IS_ERR(thread));
-		BUG_ON(vmpressure_notifier_register(&vmpressure_notif));
+	/* Init values */
+	for (i = 0; i < MAX_VICTIMS; i++) {
+		processes[i].pid = -1;
+		processes[i].uid = -1;
+		processes[i].score = -1;
 	}
+
+	for (i = 0; i < MAX_FOREGROUND; i++)
+		foreground[i] = 0;
+
+	kill_work_queue = create_workqueue("simple_lmkd");
+	queue_delayed_work(kill_work_queue, &kill_task_work, 5000);
 
 	return 0;
 }

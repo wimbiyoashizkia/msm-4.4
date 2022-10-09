0248f0e4eb3db1ead79cb588b8390e6763579e80
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-10-09 14:59:19 +0700

    msm: mdss: Remove huaqin esd-recover-command
    Subsequent to commit b334722e6cfa ("msm: mdss: Remove huaqin esd check").
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -2996,11 +2996,6 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->off_cmds,
 		"qcom,mdss-dsi-off-command", "qcom,mdss-dsi-off-command-state");
 
-#ifdef CONFIG_MACH_ASUS_SDM660
-	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->esd_recover_cmds,
-		"qcom,mdss-dsi-esd-recover-command", "qcom,mdss-dsi-esd-recover-command-state");
-#endif
-
 	rc = of_property_read_u32(np, "qcom,adjust-timer-wakeup-ms", &tmp);
 	pinfo->adjust_timer_delay_ms = (!rc ? tmp : 0);
 

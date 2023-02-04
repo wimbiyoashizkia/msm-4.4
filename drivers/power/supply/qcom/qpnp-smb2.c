61203e3d1dd75e17e3d86e73ad8f7a9807d11767
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-02-05 00:09:47 +0700

    qpnp-smb2: Set charger limit value to 100
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -2289,7 +2289,7 @@ static void smb2_create_debugfs(struct smb2 *chip)
 #ifdef CONFIG_MACH_ASUS_SDM660
 #define ATD_CHG_LIMIT_SOC	70
 int charger_limit_enable_flag;
-int charger_limit_value;
+int charger_limit_value = 100;
 static char charger_limit[8] = "0";
 static struct proc_dir_entry *limit_enable_entry;
 static struct proc_dir_entry *limit_entry;

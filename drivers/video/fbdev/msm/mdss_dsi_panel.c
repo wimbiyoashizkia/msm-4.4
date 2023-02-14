e5432115d8d03122a4030583c4a0f1fe2335be5f
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-02-14 19:16:01 +0700

    msm: mdss: Enforce power saving ULPS flags to the panels
    Better power saving during display suspend.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -2360,6 +2360,10 @@ static int mdss_dsi_parse_panel_features(struct device_node *np,
 	pinfo->dcs_cmd_by_left = of_property_read_bool(np,
 		"qcom,dcs-cmd-by-left");
 
+#ifdef CONFIG_MACH_ASUS_SDM660
+	pinfo->ulps_feature_enabled = true;
+	pinfo->ulps_suspend_enabled = true;
+#else
 	pinfo->ulps_feature_enabled = of_property_read_bool(np,
 		"qcom,ulps-enabled");
 	pr_info("%s: ulps feature %s\n", __func__,
@@ -2369,6 +2373,7 @@ static int mdss_dsi_parse_panel_features(struct device_node *np,
 		"qcom,suspend-ulps-enabled");
 	pr_info("%s: ulps during suspend feature %s", __func__,
 		(pinfo->ulps_suspend_enabled ? "enabled" : "disabled"));
+#endif
 
 	mdss_dsi_parse_dms_config(np, ctrl);
 

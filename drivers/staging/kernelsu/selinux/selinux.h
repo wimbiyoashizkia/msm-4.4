ceee787fd4d44ae3e2205459274e6d231c86c445
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-02-28 07:22:20 +0700

    staging: Import KernelSU
    A Kernel based root solution for Android devices.
    
    Features:
    1. Kernel-based su and root access management.
    2. Module system based on overlayfs.
    
    From: https://github.com/tiann/KernelSU
    Branch: main
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

---
+++ drivers/staging/kernelsu/selinux/selinux.h
@@ -0,0 +1,11 @@
+#ifndef __KSU_H_SELINUX
+#define __KSU_H_SELINUX
+
+#include "linux/types.h"
+
+void setup_selinux();
+void setenforce(bool);
+bool getenforce();
+bool is_ksu_domain();
+void apply_kernelsu_rules();
+#endif

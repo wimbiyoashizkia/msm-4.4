704989ae61b824fbbb3c7d4526c5ba2152f17175
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-06-12 20:47:44 +0700

    power: wakelock_blocker: Blacklist more wakelock
    We don't needed!
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/base/power/wakelock_blocker.h
+++ drivers/base/power/wakelock_blocker.h
@@ -3,10 +3,10 @@
  * Copyright (C) 2017, andip71 <andreasp@gmx.de>
  */
 
-#define WAKELOCK_BLOCKER_VERSION	"1.1.0"
+#define WAKELOCK_BLOCKER_VERSION    "1.1.0"
 
-#define LIST_WAKELOCK_DEFAULT				"qcom_rx_wakelock;wlan;NETLINK;[timerfd];[timerfd15_system_server];[timerfd12_system_server];[timerfd13_system_server];wlan_pno_wl;stk_input_wakelock;hal_bluetooth_lock;IPCRTR_lpass_rx;alarmtimer;radio-interface;bluetooth_timer;poll-wake-lock;pil-a512_zap;event3;event1;event6;pil-modem;vbus-soc:usb_nop_phy;qcril_pre_client_init;a800000.ssusb;800f000.qcom,spmi:qcom,pm660@0:qpnp,fg;tftp_server_wakelock;vdev_set_key;vdev_start;wlan_wow_wl;wlan_ap_assoc_lost_wl;usb;battery;800f000.qcom,spmi:qcom,pm660@0:qcom,qpnp-smb2;event0;800f000.qcom,spmi:qcom,pm660l@3:analog-codec@f000;qcril;video3;ipc000000a4_1385_NasModemEndPoi;ipc000000a5_982_NasModemEndPoi;ipc000000b5_1932_NasModemEndPoi;ipc000000bf_840_LocApiMsgTask;IPCRTR_mpss_rx;mpss_IPCRTR;soc:gpio_keys;CHG_PLCY_HVDCP_WL;CHG_PLCY_HVDCP2_WL;CHG_PLCY_MAIN_WL;CHG_PLCY_STD_PD_WL;CHG_PLCY_PPS_WL;SensorService_wakelock;ipc000000e2_2029_NasModemEndPoi;deleted;1344;1338"
+#define LIST_WAKELOCK_DEFAULT               "qcom_rx_wakelock;wlan;NETLINK;[timerfd];[timerfd15_system_server];[timerfd12_system_server];[timerfd13_system_server];wlan_pno_wl;stk_input_wakelock;hal_bluetooth_lock;IPCRTR_lpass_rx;alarmtimer;radio-interface;bluetooth_timer;poll-wake-lock;pil-a512_zap;event3;event1;event6;pil-modem;vbus-soc:usb_nop_phy;qcril_pre_client_init;a800000.ssusb;800f000.qcom,spmi:qcom,pm660@0:qpnp,fg;tftp_server_wakelock;vdev_set_key;vdev_start;wlan_wow_wl;wlan_ap_assoc_lost_wl;usb;battery;800f000.qcom,spmi:qcom,pm660@0:qcom,qpnp-smb2;event0;800f000.qcom,spmi:qcom,pm660l@3:analog-codec@f000;qcril;video3;video4;rmt_storage_483825372496;rmt_storage_483807558992;ipc00000024_756_rmt_storage;ipc00000090_1256_imsdatadaemon;ipc000000b3_1988_NasModemEndPoi;ipc0000002b_762_tftp_server;ipc000000c2_802_LocApiMsgTask;ipc000000a4_1385_NasModemEndPoi;ipc000000a5_982_NasModemEndPoi;ipc000000b5_1932_NasModemEndPoi;ipc000000bf_840_LocApiMsgTask;IPCRTR_mpss_rx;mpss_IPCRTR;soc:gpio_keys;CHG_PLCY_HVDCP_WL;CHG_PLCY_HVDCP2_WL;CHG_PLCY_MAIN_WL;CHG_PLCY_STD_PD_WL;CHG_PLCY_PPS_WL;SensorService_wakelock;ipc000000e2_2029_NasModemEndPoi;deleted;1344;1338"
 
-#define LENGTH_LIST_WAKELOCK				4096
-#define LENGTH_LIST_WAKELOCK_DEFAULT		512
-#define LENGTH_LIST_WAKELOCK_SEARCH		LENGTH_LIST_WAKELOCK + LENGTH_LIST_WAKELOCK_DEFAULT + 5
+#define LENGTH_LIST_WAKELOCK                4096
+#define LENGTH_LIST_WAKELOCK_DEFAULT        512
+#define LENGTH_LIST_WAKELOCK_SEARCH     LENGTH_LIST_WAKELOCK + LENGTH_LIST_WAKELOCK_DEFAULT + 5

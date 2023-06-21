1849ba244d0f0d2832fcd1b5ed1845581fd00611
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-06-21 22:39:35 +0700

    Revert "power: qpnp-smb2: Implement battery charging_enabled node"
    ASUS driver already had this. Remove it!
    
    errors:
    ../drivers/power/supply/qcom/qpnp-smb2.c: In function 'smb2_batt_get_prop':
    ../drivers/power/supply/qcom/qpnp-smb2.c:1007:9: error: duplicate case value
     1007 |         case POWER_SUPPLY_PROP_CHARGING_ENABLED:
          |         ^~~~
    ../drivers/power/supply/qcom/qpnp-smb2.c:1000:9: note: previously used here
     1000 |         case POWER_SUPPLY_PROP_CHARGING_ENABLED:
          |         ^~~~
    ../drivers/power/supply/qcom/qpnp-smb2.c: In function 'smb2_batt_set_prop':
    ../drivers/power/supply/qcom/qpnp-smb2.c:1133:9: error: duplicate case value
     1133 |         case POWER_SUPPLY_PROP_CHARGING_ENABLED:
          |         ^~~~
    ../drivers/power/supply/qcom/qpnp-smb2.c:1126:9: note: previously used here
     1126 |         case POWER_SUPPLY_PROP_CHARGING_ENABLED:
          |         ^~~~
    
    This reverts commit 094b738f46c80c56d03d923c3e780e071abe34b0.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -942,7 +942,6 @@ static int smb2_init_dc_psy(struct smb2 *chip)
  *************************/
 
 static enum power_supply_property smb2_batt_props[] = {
-	POWER_SUPPLY_PROP_CHARGING_ENABLED,
 	POWER_SUPPLY_PROP_INPUT_SUSPEND,
 	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_HEALTH,
@@ -996,9 +995,6 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_PRESENT:
 		rc = smblib_get_prop_batt_present(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
-		val->intval = !get_effective_result(chg->chg_disable_votable);
 		break;
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		rc = smblib_get_prop_input_suspend(chg, val);
@@ -1123,9 +1119,6 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 	union power_supply_propval pval = {0, };
 
 	switch (prop) {
-	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
-		vote(chg->chg_disable_votable, USER_VOTER, !!!val->intval, 0);
-		break;
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		rc = smblib_set_prop_input_suspend(chg, val);
 		break;
@@ -1212,7 +1205,6 @@ static int smb2_batt_prop_is_writeable(struct power_supply *psy,
 		enum power_supply_property psp)
 {
 	switch (psp) {
-	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
 	case POWER_SUPPLY_PROP_CAPACITY:

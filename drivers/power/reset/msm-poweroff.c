3cdeb343bb7af0a436447678358f5ad7c2dd6b0b
Sultan Alsawaf <sultanxda@gmail.com>
2023-09-16 18:41:19 +0700

    power: reset: msm-poweroff: Always perform warm reset
    To preserve console-ramoops across a reboot.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -305,6 +305,9 @@ static void msm_restart_prepare(const char *cmd)
 		in_panic = false;
 	}
 
+	/* To preserve console-ramoops */
+	need_warm_reset = true;
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);

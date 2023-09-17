wimbiyoashizkia/msm-4.4

commit d160728f4ec33938341d3fd7294236ce111dd061

power: reset: msm-poweroff: Avoid entering download mode
With this patch, kernel panics now reboot the phone gracefully.

Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

drivers/power/reset/msm-poweroff.c
@@ -280,8 +280,7 @@ static void msm_restart_prepare(const char *cmd)
 	 * Kill download mode if master-kill switch is set
 	 */
 
-	set_dload_mode(download_mode &&
-			(in_panic || restart_mode == RESTART_DLOAD));
+	set_dload_mode(false);
 #endif
 
 	if (qpnp_pon_check_hard_reset_stored()) {

commit 9fa4e3fcf628e0a6b9e53bd9546773786e673956

power: reset: msm-poweroff: Do not reboot to EDL
In case of kernel panic, this retains EDL funcitonality.
Can still get there via ADB/button combination but still
disables kernel panic to EDL.

Fixes: d160728f4ec33938341d3fd7294236ce111dd061 ("power: reset: msm-poweroff: Avoid entering download mode")
Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

drivers/power/reset/msm-poweroff.c
@@ -280,7 +280,7 @@ static void msm_restart_prepare(const char *cmd)
 	 * Kill download mode if master-kill switch is set
 	 */
 
-	set_dload_mode(false);
+	set_dload_mode(download_mode && restart_mode == RESTART_DLOAD);
 #endif
 
 	if (qpnp_pon_check_hard_reset_stored()) {
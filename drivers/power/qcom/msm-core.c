618cd7cca172b63f6982aa764b5ac209c3d9e87d
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-10-16 07:38:26 +0700

    power: qcom: msm-core: Reduce temp-base & increase temp-max point
    Better for repopulate stats on cpus.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -38,8 +38,8 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/trace_msm_core.h>
 
-#define TEMP_BASE_POINT 35
-#define TEMP_MAX_POINT 95
+#define TEMP_BASE_POINT 10
+#define TEMP_MAX_POINT 200
 #define CPU_HOTPLUG_LIMIT 80
 #define CPU_BIT_MASK(cpu) BIT(cpu)
 #define DEFAULT_LOW_HYST_TEMP 10

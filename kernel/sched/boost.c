wimbiyoashizkia/msm-4.4

commit ea6cb3d193ee632313237e035b82603fc0eac1bc

sched: boost: Perform SchedTune boosting when sched_boost is triggered
Boost top-app SchedTune tasks using the dynamic_boost value when
/proc/sys/kernel/sched_boost is activated. This is usually triggered by
CAF's perf daemon.

Signed-off-by: joshuous <joshuous@gmail.com>
Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

kernel/sched/boost.c
@@ -200,6 +200,11 @@ int sched_boost_handler(struct ctl_table *table, int write,
 		goto done;
 
 	if (verify_boost_params(old_val, *data)) {
+		if (*data > 0)
+			stune_boost("top-app");
+		else
+			reset_stune_boost("top-app");
+
 		_sched_set_boost(old_val, *data);
 	} else {
 		/*

commit 0f51b14ab2ef88cdb722450b552c57fad6a730a0

sched: boost: Guard a DYNAMIC_STUNE_BOOST on sched_boost_handler initialization
This workqueue wasn't guarded properly on DYNAMIC_STUNE_BOOST Kconfig.

Fixes: ea6cb3d193ee632313237e035b82603fc0eac1bc ("sched: boost: Perform SchedTune boosting when sched_boost is triggered").
Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

kernel/sched/boost.c
@@ -204,10 +204,12 @@ int sched_boost_handler(struct ctl_table *table, int write,
 		goto done;
 
 	if (verify_boost_params(old_val, *data)) {
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 		if (*data > 0)
 			do_stune_sched_boost("top-app", &boost_slot);
 		else
 			reset_stune_boost("top-app", boost_slot);
+#endif
 
 		_sched_set_boost(old_val, *data);
 	} else {
c526c40f4726b85908aa3977f83bff098cce2d2a
Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>
2023-12-09 13:53:32 +0700

    fair: BORE
    Signed-off-by: Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -180,7 +180,7 @@ penalty_scale_mayhalve(u64 delta, struct sched_entity *se, bool half)
 	if (half)
 		score >>= 1;
 
-	return mul_u64_u32_shr(delta, sched_prio_to_wmult[score], 22);
+	return mul_u64_u32_shr(delta, prio_to_wmult[score], 22);
 }
 
 #define penalty_scale(delta, se) \
